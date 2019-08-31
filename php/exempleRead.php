202ec5c31c1dbda6173b5b3de2014ac7a6c0f0ae
Yannick Benito <benitoyannick7@gmail.com>
2019-08-31 15:08:39 +0200

    ok

--- php/exempleRead.php
+++ php/exempleRead.php
@@ -11,8 +11,8 @@ $id=$_GET['id'];
 
 $user = $userMan->read($id);
 $results=$dataManager->readWhereFkCommercial($user);
-// echo 'mission';
-// var_dump($results);
+ echo 'mission';
+var_dump($results);
 
 // header('Content-Type:application/json');
-var_dump($results);
+
